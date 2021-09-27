4f1a942a95e92f1509779e03f96670f81845c7f0
Rifat Ozen <rftozn000@gmail.com>
2021-09-27 15:18:58 +0300

    Homework finished.

---
+++ js/clock.js
@@ -0,0 +1,44 @@
+let username = prompt("Adınız Nedir");
+document.querySelector("#myName").innerHTML = username;
+
+function Clock() {
+  let currDate = new Date();
+
+  let currHour = currDate.getHours();
+  let currMinute = currDate.getMinutes();
+  let currSecond = currDate.getSeconds();
+  let currDay = currDate.getDay();
+
+  switch (currDay) {
+    case 0:
+      currDay = "Pazar";
+      break;
+    case 1:
+      currDay = "Pazartesi";
+      break;
+    case 2:
+      currDay = "Salı";
+      break;
+    case 3:
+      currDay = "Çarşamba";
+      break;
+    case 4:
+      currDay = "Perşembe";
+      break;
+    case 5:
+      currDay = "Cuma";
+      break;
+    case 6:
+      currDay = "Cumartesi";
+      break;
+  }
+  currHour = currHour < 10 ? "0" + currHour : currHour;
+  currMinute = currMinute < 10 ? "0" + currMinute : currMinute;
+  currSecond = currSecond < 10 ? "0" + currSecond : currSecond;
+
+  let date = `${currHour}:${currMinute}:${currSecond} ${currDay}`;
+
+  document.querySelector("#myClock").innerHTML = date;
+}
+
+setInterval("Clock()", 1000);
